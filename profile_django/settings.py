0917e02de67776a62ed16256cceb02905154caa3
Pacharapon21 <aszxcvbnm1000@gmail.com>
2025-08-17 13:41:34 +0700

    update config

--- profile_django/settings.py
+++ profile_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xfky9d8sasp!0p+hc9d@rjt@p5&s983bh0)d%zyan1b2nzy(ql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
